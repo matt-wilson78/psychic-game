9b34b83e6611eb6787003f7bd5336fb71d4a3ff5
Matt Wilson <matt@firstflightagency.com>
2019-08-23 09:11:11 -0400

    testing functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,27 @@
-<script>
-
     var wins = 0;
     var losses = 0;
     var guessesLeft = 9;
     var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    var computerGuess = alphabet[Math.floor(Math.random () * alphabet.length)];
-    var changeWins = wins++;
-    var changeLosses = losses++;
-    var changeGuesses = guessesLeft--
     var lettersGuessed = [];
     
-    document.onKeyUp = function(event) {
+    document.onkeyup = function(event) {
         var userGuess = event.key;
 
-        if (userGuess === computerGuess); {
-           wins++ 
+        var computerGuess = alphabet[Math.floor(Math.random () * alphabet.length)];
+
+        //if (userGuess === computerGuess) {
+           //wins++;
+            //} else { 
+                //guessesLeft--;
+                //} if (guessesLeft === 0) {
+                    //losses++;
+                    //}
+        alert("User guess: " + userGuess);
+        alert("Computer Letter: " + computerGuess);
         }
-        else (guessesLeft--);
-        if (guessesLeft === 0); {
-            losses++
-        }
-    }
+    
 
     
     
 
 
-</script>
