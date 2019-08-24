0646af2120de4597d1ca3fe9089ef923d42364aa
Matt Wilson <matt@firstflightagency.com>
2019-08-24 19:50:59 -0400

    still trying to figure out why wins losses etc aren't showing up outside console

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,5 @@
+    
+    
     //Statistic variable
     var wins = 0;
     var losses = 0;
@@ -18,35 +20,41 @@
     //Generate users pick
     document.onkeyup = function(event) {
         var userPick = event.key;
+        lettersGuessed.push(userPick);
 
         //Loop to determine wins, losses, and guesses left
         if (userPick === computerPick) {
             wins++;
             computerPick = alphabet[Math.floor(Math.random () * alphabet.length)];
+            console.log("CPU pick: " + computerPick);
             guessesLeft = 9;
+            lettersGuessed = [];
             } else { 
                 guessesLeft--;
             }
         console.log(userPick);
             //Hide directions
             //directions.textContent = "";
-
-        //Display wins, losses, guesses left, and letters guessed
-            postWins.innerHtml = "Wins: " + wins;
-            console.log("Wins: " + wins);
-            postLosses.innerHtml = "Losses: " + losses;
-            console.log("Losses: " + losses);
-            postGuessesLeft.innerHtml = "Guesses Left" + guessesLeft;
-            console.log("Guesses Left: " + guessesLeft)
-            postGuessesSoFar.innerHtml = "Letters Guessed: " + lettersGuessed;
-            console.log("Letters Guessed: " + lettersGuessed);
         
         if (guessesLeft === 0) {
             losses++
             computerPick = alphabet[Math.floor(Math.random () * alphabet.length)];
+            console.log("CPU Pick: " + computerPick);
             guessesLeft = 9;
-            }    
-    }
+            lettersGuessed = [];
+            }
+            
+        //Display wins, losses, guesses left, and letters guessed
+        postWins.innerHtml = "Wins: " + wins;
+        console.log("Wins: " + wins);
+        postLosses.innerHtml = "Losses: " + losses;
+        console.log("Losses: " + losses);
+        postGuessesLeft.innerHtml = "Guesses Left" + guessesLeft;
+        console.log("Guesses Left: " + guessesLeft)
+        postGuessesSoFar.innerHtml = "Letters Guessed: " + lettersGuessed;
+        console.log("Letters Guessed: " + lettersGuessed);
+
+    };
     
 
     
