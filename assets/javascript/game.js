422fe9f063fe767e51a7fdb593d8f3c3042a55e2
Matt Wilson <matt@firstflightagency.com>
2019-08-23 21:18:44 -0400

    Wins, Losses, cpu and user choices, letters guessed all console logging now

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,46 @@
+    //Statistic variable
     var wins = 0;
     var losses = 0;
     var guessesLeft = 9;
     var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     var lettersGuessed = [];
-    
+
+    //Computer choice variable
+    var computerPick = alphabet[Math.floor(Math.random () * alphabet.length)];
+
+    //Display variables
+    var directions = document.getElementById("directions-text");
+    var postWins = document.getElementById("wins");
+    var postLosses = document.getElementById("losses");
+    var postGuessesLeft = document.getElementById("guessesLeft");
+    var postGuessesSoFar = document.getElementById("guessesSoFar")
+
+    //Generate users pick
     document.onkeyup = function(event) {
-        var userGuess = event.key;
+        var userPick = event.key;
 
-        var computerGuess = alphabet[Math.floor(Math.random () * alphabet.length)];
+        //Loop to determine wins, losses, and guesses left
+        if (userPick === computerPick) {
+            wins++;
+            } else { 
+                guessesLeft--;
+            }
+        console.log(userPick);
+        console.log(computerPick);
+            //Hide directions
+            //directions.textContent = "";
 
-        //if (userGuess === computerGuess) {
-           //wins++;
-            //} else { 
-                //guessesLeft--;
-                //} if (guessesLeft === 0) {
-                    //losses++;
-                    //}
-        alert("User guess: " + userGuess);
-        alert("Computer Letter: " + computerGuess);
-        }
+        //Display wins, losses, guesses left, and letters guessed
+            postWins.innerHtml = "Wins: " + wins;
+            console.log("Wins: " + wins);
+            postLosses.innerHtml = "Losses: " + losses;
+            console.log("Losses: " + losses);
+            postGuessesLeft.innerHtml = "Guesses Left" + guessesLeft;
+            console.log("Guesses Left: " + guessesLeft)
+            postGuessesSoFar.innerHtml = "Letters Guessed: " + lettersGuessed;
+            console.log("Letters Guessed: " + lettersGuessed);
+                
+    }
     
 
     
