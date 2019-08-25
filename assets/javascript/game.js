9fe3fa207f859f8d4c04c7726568de4973cc9c9c
Matt Wilson <matt@firstflightagency.com>
2019-08-24 21:09:33 -0400

    It works!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,12 +11,11 @@
     var computerPick = alphabet[Math.floor(Math.random () * alphabet.length)];
 
     //Display variables
-    var directions = document.getElementById("directions-text");
     var postWins = document.getElementById("wins");
     var postLosses = document.getElementById("losses");
     var postGuessesLeft = document.getElementById("guessesLeft");
     var postGuessesSoFar = document.getElementById("guessesSoFar")
-    console.log("CPU pick: " + computerPick);
+
     //Generate users pick
     document.onkeyup = function(event) {
         var userPick = event.key;
@@ -26,33 +25,24 @@
         if (userPick === computerPick) {
             wins++;
             computerPick = alphabet[Math.floor(Math.random () * alphabet.length)];
-            console.log("CPU pick: " + computerPick);
             guessesLeft = 9;
             lettersGuessed = [];
             } else { 
                 guessesLeft--;
             }
-        console.log(userPick);
-            //Hide directions
-            //directions.textContent = "";
         
         if (guessesLeft === 0) {
             losses++
             computerPick = alphabet[Math.floor(Math.random () * alphabet.length)];
-            console.log("CPU Pick: " + computerPick);
             guessesLeft = 9;
             lettersGuessed = [];
             }
             
         //Display wins, losses, guesses left, and letters guessed
-        postWins.innerHtml = "Wins: " + wins;
-        console.log("Wins: " + wins);
-        postLosses.innerHtml = "Losses: " + losses;
-        console.log("Losses: " + losses);
-        postGuessesLeft.innerHtml = "Guesses Left" + guessesLeft;
-        console.log("Guesses Left: " + guessesLeft)
-        postGuessesSoFar.innerHtml = "Letters Guessed: " + lettersGuessed;
-        console.log("Letters Guessed: " + lettersGuessed);
+        postWins.innerHTML = "Wins: " + wins;
+        postLosses.innerHTML = "Losses: " + losses;
+        postGuessesLeft.innerHTML = "Guesses Left: " + guessesLeft;
+        postGuessesSoFar.innerHTML = "Letters Guessed: " + lettersGuessed;
 
     };
     
